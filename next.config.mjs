88648e732655f92a044e636d82623213ba5e531a
nnoromiv <nnorom.prince44@gmail.com>
2024-01-26 11:15:28 +0100

    Next.Js Conf.

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,7 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    exportPathMap: function () {
-        return {
-            '/': { page: '/' },
-            // Add other pages as needed
-        };
-    },
+    output: 'export',
 };
 
 export default nextConfig;
